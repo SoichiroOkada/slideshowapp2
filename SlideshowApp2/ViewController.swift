d3ec7438774581343ebe77ceebe28431d621075f
soichiro.okada <straycat0914@gmail.com>
2020-11-21 19:18:02 +0900

    画面遷移作成中

--- SlideshowApp2/ViewController.swift
+++ SlideshowApp2/ViewController.swift
@@ -9,15 +9,63 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    var img0 = UIImage(named:"IMG0")
+    
+    
+    @IBAction func viewTap(_ sender: Any) { 
+    }
     @IBOutlet weak var imageView: UIImageView!
+
+    @IBAction func modoru(_ sender: Any) {
+        // 表示している画像の番号を1減らす
+        displayImageNumber -= 1
+
+        // 表示している画像の番号を元に画像を表示する
+        displayImage()
+    }
+    @IBAction func susumu(_ sender: Any) {
+        // 表示している画像の番号を1増やす
+         displayImageNumber += 1
+
+         // 表示している画像の番号を元に画像を表示する
+         displayImage()}
+    
+    var displayImageNumber = 0
+    
+    func displayImage() {
+    // 画像の名前の配列
+        let imageNameArray = [
+            "0",
+            "1",
+            "2",
+        ]
+        // 範囲より下を指している場合、最後の画像を表示
+        if displayImageNumber < 0 {
+           displayImageNumber = 2
+        }
+
+        // 範囲より上を指している場合、最初の画像を表示
+        if displayImageNumber > 2 {
+           displayImageNumber = 0
+        }
+
+        // 表示している画像の番号から名前を取り出し
+        let name = imageNameArray[displayImageNumber]
+
+        // 画像を読み込み
+        let image = UIImage(named: name)
+
+        // Image Viewに読み込んだ画像をセット
+        imageView.image = image
+    }
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        imageView.image = img0
+        self.performSegue(withIdentifier: "tozoomView", sender: nil)
         
+        @IBAction func unwind(_ segue: UIStoryboardSegue)
+
         // Do any additional setup after loading the view.
     }
 
---
+++ SlideshowApp2/zoomView.swift
@@ -0,0 +1,30 @@
+//
+//  zoomView.swift
+//  SlideshowApp2
+//
+//  Created by straycat on 2020/11/21.
+//  Copyright © 2020 soichiro.okada. All rights reserved.
+//
+
+import UIKit
+
+class zoomView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
