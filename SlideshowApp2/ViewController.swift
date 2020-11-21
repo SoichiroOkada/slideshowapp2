8c719d3a8890dbc0c05b561f9cf92864fad365d6
soichiro.okada <straycat0914@gmail.com>
2020-11-22 00:16:14 +0900

    2020.11.22 0:30

--- SlideshowApp2/ViewController.swift
+++ SlideshowApp2/ViewController.swift
@@ -9,13 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
+
     
-    
+    var timer_sec: Float = 0
+    @objc func updateTimer(_ timer: Timer){
+        displayImageNumber += 1
+        displayImage()
+        
+        func displayImage() {
+        // 画像の名前の配列
+            let imageNameArray = [
+                "0",
+                "1",
+                "2",
+            ]
+            // 範囲より下を指している場合、最後の画像を表示
+            if displayImageNumber < 0 {
+               displayImageNumber = 2
+            }
+
+            // 範囲より上を指している場合、最初の画像を表示
+            if displayImageNumber > 2 {
+               displayImageNumber = 0
+            }
+        // 表示している画像の番号から名前を取り出し
+        let name = imageNameArray[displayImageNumber]
+
+        // 画像を読み込み
+        let image = UIImage(named: name)
+
+          imageView.image = image
+    }
+    }
     @IBAction func viewTap(_ sender: Any) { 
     }
     @IBOutlet weak var imageView: UIImageView!
 
-
+    @IBOutlet weak var buttun1: NSLayoutConstraint!
+    @IBAction func saisei(_ sender: Any) {
+        Timer.SchedulerTimer(timeInterval: 2.0,target: self, selector:#selector(updateTimer(_:)),userInfo: nil, repeats: true)
+    }
+    
     @IBAction func modoru(_ sender: Any) {
         // 表示している画像の番号を1減らす
         displayImageNumber -= 1
@@ -59,11 +93,15 @@ class ViewController: UIViewController {
         imageView.image = image
     }
 
+    @IBAction func tapgesture(_ sender: Any) {
+            self.performSegue(withIdentifier: "tozoomView", sender: nil)
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.performSegue(withIdentifier: "tozoomView", sender: nil)
+
         
         // Do any additional setup after loading the view.
  
