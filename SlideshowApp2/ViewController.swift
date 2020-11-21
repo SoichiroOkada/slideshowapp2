80d8e4f1c9cbccae02e43541c2286ee0d277e56d
soichiro.okada <straycat0914@gmail.com>
2020-11-21 13:19:44 +0900

    Initial Commit

---
+++ SlideshowApp2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp2
+//
+//  Created by straycat on 2020/11/21.
+//  Copyright © 2020 soichiro.okada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
