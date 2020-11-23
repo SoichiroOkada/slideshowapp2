05bb3e1e842d3d795aa05346b924914ebad5fb4c
soichiro.okada <straycat0914@gmail.com>
2020-11-23 16:13:59 +0900

    timerを作る

--- SlideshowApp2/ViewController.swift
+++ SlideshowApp2/ViewController.swift
@@ -15,43 +15,17 @@ class ViewController: UIViewController {
 
     }
     
-    var timer_sec: Float = 0
-    @objc func updateTimer(_ timer: Timer){
-        displayImageNumber += 1
-        displayImage()
-        
-        func displayImage() {
-        // 画像の名前の配列
-            let imageNameArray = [
-                "0",
-                "1",
-                "2",
-            ]
-            // 範囲より下を指している場合、最後の画像を表示
-            if displayImageNumber < 0 {
-               displayImageNumber = 2
-            }
-
-            // 範囲より上を指している場合、最初の画像を表示
-            if displayImageNumber > 2 {
-               displayImageNumber = 0
-            }
-        // 表示している画像の番号から名前を取り出し
-        let name = imageNameArray[displayImageNumber]
-
-        // 画像を読み込み
-        let image = UIImage(named: name)
-
-          imageView.image = image
-    }
-    }
+    var timer_sec: Int = 0
+    
+    
     @IBAction func viewTap(_ sender: Any) { 
     }
     @IBOutlet weak var imageView: UIImageView!
 
     @IBOutlet weak var buttun1: NSLayoutConstraint!
     @IBAction func saisei(_ sender: Any) {
-            }
+    createTimer()
+    }
     
     @IBAction func modoru(_ sender: Any) {
         // 表示している画像の番号を1減らす
@@ -100,12 +74,27 @@ class ViewController: UIViewController {
             self.performSegue(withIdentifier: "tozoomView", sender: nil)
         
     }
+    var timer: Timer!
+    func createTimer(){
+        timer = Timer.scheduledTimer(timeInterval: 2.0,
+                                     target: self,
+            selector:#selector(updateTimer(_:)),
+            useInfo:nil,
+            repeats:true)
+    }
+    @objc func updateTimer(_ timer: Timer){
+                 displayImageNumber += 1
+                displayImage()}
+    
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-
+        imageView.image = UIImage(named:"0")
         
+
+
         // Do any additional setup after loading the view.
  
 }
